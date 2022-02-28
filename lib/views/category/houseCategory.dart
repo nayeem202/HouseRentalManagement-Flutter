42994821bdc2ddce917d5dc7854a4f29b3e5b909
Nayeem <nayeem.ahmedemba@gmail.com>
2022-02-28 22:06:21 +0600

    categories

---
+++ lib/views/category/houseCategory.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'categorygriddashboard.dart';
+
+
+class Category extends StatefulWidget {
+  @override
+  CategoryState createState() => new CategoryState();
+}
+
+class CategoryState extends State<Category> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 16, right: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+
+                    // SizedBox(
+                    //   height: 4,
+                    // ),
+                    Text(
+                      "Categories",
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                              color: Color(0xffa29aac),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600)),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  alignment: Alignment.topCenter,
+                  icon: Image.asset(
+                    "assets/notification.png",
+                    width: 24,
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+         GridDashboard()
+        ],
+      ),
+    );
+  }
+}
