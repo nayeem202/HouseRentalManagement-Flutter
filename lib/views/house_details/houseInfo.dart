250019855d94714acfed5239d5499ff7453bd704
Nayeem <nayeem.ahmedemba@gmail.com>
2022-03-05 01:28:31 +0600

    details page 2

--- lib/views/house_details/houseInfo.dart
+++ lib/views/house_details/houseInfo.dart
@@ -3,11 +3,11 @@ import 'package:client_mobile/ep882_house_rental_app/model/AdvertiseModel.dart';
 import 'package:client_mobile/views/advertisingDetails/components/carousel_images.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 import '../advertisingDetails/components/constants.dart';
 
 class HouseInfo extends StatefulWidget {
-
   final advertisingId;
   final location;
   final type;
@@ -32,30 +32,48 @@ class HouseInfo extends StatefulWidget {
   final phone;
   final email;
 
-
-
-
-
-
-
   //HouseInfo({Key? key, required int advertisingId, required String location, required String status, required String type, required int bedrooms, required int bathrooms, required int price, required int sqft, required String additionalinformation, required String images,required String imagesUri, required String img2, required String imgUri2, required String img3, required String imgUri3, required String video, required String videoType, required double lat, required double lng}) : super(key: key);
-  const HouseInfo({
-    this.location, this.name, this.username, this.phone, this.email, this.advertisingId, this.type, this.status, this.bedrooms, this.bathrooms, this.price, this.sqft, this.additionalinformation, this.images, this.imagesUri, this.img2, this.imgUri2, this.img3, this.imgUri3, this.video, this.videoType, this.lat, this.lng
-});
+  const HouseInfo(
+      {this.location,
+      this.name,
+      this.username,
+      this.phone,
+      this.email,
+      this.advertisingId,
+      this.type,
+      this.status,
+      this.bedrooms,
+      this.bathrooms,
+      this.price,
+      this.sqft,
+      this.additionalinformation,
+      this.images,
+      this.imagesUri,
+      this.img2,
+      this.imgUri2,
+      this.img3,
+      this.imgUri3,
+      this.video,
+      this.videoType,
+      this.lat,
+      this.lng});
 
   @override
   _HouseInfoState createState() => _HouseInfoState();
 }
 
 class _HouseInfoState extends State<HouseInfo> {
+  late VideoPlayerController _controller;
 
-
-
-    @override
-    void initState(){
-
-    }
-
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network("${widget.video}")
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,7 +91,7 @@ class _HouseInfoState extends State<HouseInfo> {
                   dotSize: 5,
                   dotBgColor: Colors.transparent,
                   autoplay: true,
-                  images:  [
+                  images: [
                     NetworkImage(
                       "${widget.imagesUri}",
                     ),
@@ -83,15 +101,12 @@ class _HouseInfoState extends State<HouseInfo> {
                     NetworkImage(
                       "${widget.imgUri3}",
                     ),
-
                   ],
                 ),
               ),
-            ]
-            ),
-            Expanded(child:
-
-            ListView(
+            ]),
+            Expanded(
+                child: ListView(
               physics: BouncingScrollPhysics(),
               shrinkWrap: true,
               children: [
@@ -128,7 +143,7 @@ class _HouseInfoState extends State<HouseInfo> {
                         ],
                       ),
                       Text(
-                        ("House Type - ${widget.type}"),
+                        ("${widget.type}"),
                         style: TextStyle(
                           fontSize: 15,
                           fontWeight: FontWeight.bold,
@@ -138,8 +153,8 @@ class _HouseInfoState extends State<HouseInfo> {
                   ),
                 ),
                 Padding(
-                  padding:
-                  const EdgeInsets.only(left: appPadding, bottom: appPadding),
+                  padding: const EdgeInsets.only(
+                      left: appPadding, bottom: appPadding),
                   child: Text(
                     ("Additional Information"),
                     style: TextStyle(
@@ -256,13 +271,7 @@ class _HouseInfoState extends State<HouseInfo> {
                               SizedBox(
                                 height: 10,
                               ),
-                              Text(
-                                '',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
+
                             ],
                           ),
                         ),
@@ -274,15 +283,12 @@ class _HouseInfoState extends State<HouseInfo> {
                         ),
                         child: Container(
                           width: 100,
-
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-
                               SizedBox(
                                 height: 10,
                               ),
-
                             ],
                           ),
                         ),
@@ -294,7 +300,7 @@ class _HouseInfoState extends State<HouseInfo> {
                   padding: EdgeInsets.only(
                     left: appPadding,
                     right: appPadding,
-                    bottom: appPadding * 4,
+                    bottom: appPadding,
                   ),
                   child: Text(
                     ("Description - ${widget.additionalinformation}"),
@@ -304,11 +310,46 @@ class _HouseInfoState extends State<HouseInfo> {
                       height: 1.5,
                     ),
                   ),
-                )
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: appPadding,
+                    bottom: appPadding
+                  ),
+                  child: Text(
+                    'Virtual Tour',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: _controller.value.isInitialized
+                      ? AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(_controller),
+                        )
+                      : Container(),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: FloatingActionButton(
+                      onPressed: () {
+                        setState(() {
+                          _controller.value.isPlaying
+                              ? _controller.pause()
+                              : _controller.play();
+                        });
+                      },
+                      child: Icon(
+                        _controller.value.isPlaying
+                            ? Icons.pause
+                            : Icons.play_arrow,
+                      )),
+                ),
               ],
-            )
-            )
-
+            ))
           ],
         )
       ],
