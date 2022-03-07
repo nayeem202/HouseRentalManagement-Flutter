2c817493c17a0153721eaf7e6510ed3f6d349d36
Nayeem <nayeem.ahmedemba@gmail.com>
2022-03-07 17:01:46 +0600

    mail service

--- lib/helper/constant.dart
+++ lib/helper/constant.dart
@@ -5,3 +5,4 @@ final String getAllAdvertisingApi = host+"/getAddvertising";
 final String getAdvertisingBySearchApi = host+"/getAddvertisingBySearch/";
 final String getAdvertisingByCategory = host+"/getAddvertisingByType/";
 final String getUserinfo = host + "/getUserInfo/";
+final String sendMailUri = host + "/send_amail";
--- lib/views/house_details/bottom_buttons.dart
+++ lib/views/house_details/bottom_buttons.dart
@@ -1,12 +1,19 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:client_mobile/helper/constant.dart';
+import 'package:http/http.dart' as http;
 import 'package:client_mobile/views/house_details/houseInfo.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import '../../helper/http_helper.dart';
 import '../advertisingDetails/components/constants.dart';
 //import 'contactBox.dart';
 
 class BottomButtons extends StatelessWidget {
-  final TextEditingController _textEditingControllername = TextEditingController();
-  final TextEditingController _textEditingControlleremail = TextEditingController();
-  final TextEditingController _textEditingControllermessage = TextEditingController();
+  final  _textEditingControllername = TextEditingController();
+  final  _textEditingControlleremail = TextEditingController();
+  final  _textEditingControllermessage = TextEditingController();
+  final _http = HttpHelper();
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
@@ -14,9 +21,59 @@ class BottomButtons extends StatelessWidget {
   var email;
   var phone;
 
-  //final username = const HouseInfo();
+  sendMail() async{
+    String to = email;
+    String subject = "Someone wants to connect with you for rent";
+   //List<String> prices=[_textEditingControllername, _textEditingControlleremail, _textEditingControllermessage];
 
-  //var username= HouseInfo(username: "username",);
+
+
+    try{
+      var map = new Map<String, dynamic>();
+      map["to"] = to;
+      map["subject"] = subject;
+      map["message"] = _textEditingControllermessage.text;
+
+      final response = await _http.postData(sendMailUri,  jsonEncode(map));
+      if (response.statusCode == 200){
+        print(jsonEncode(map));
+        Fluttertoast.showToast(
+            msg: "Sent Message Successfully",
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0
+        );
+      }
+      else{
+        Fluttertoast.showToast(
+            msg: "Sent Message Failed",
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0
+        );
+      }
+
+
+
+    }catch(e){
+      log(e.toString());
+      Fluttertoast.showToast(
+          msg: "$e",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    }
+  }
 
   BottomButtons({required this.name, required this.email, required this.phone});
 
@@ -48,148 +105,143 @@ class BottomButtons extends StatelessWidget {
                 showDialog(
                     context: context,
                     builder: (BuildContext context) {
-                      return SingleChildScrollView(
-                        child: AlertDialog(
-                          content: Form(
-                              key: _formKey,
-                              child: Container(
-                                width: 10000,
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Padding(
-                                      padding: EdgeInsets.only(bottom: 0),
-                                      child: RichText(
-                                        text: TextSpan(
-                                          children: [
-                                            WidgetSpan(
-                                              child: Icon(
-                                                Icons.person,
-                                                size: 14,
-                                                color: Colors.deepPurple,
-                                              ),
-                                            ),
-                                            TextSpan(
-                                              text: ("  Owner Name- " + name),
-                                              style: TextStyle(
-                                                  color: Colors.black87,
-                                                  fontSize: 15,
-                                                  fontWeight: FontWeight.normal),
-                                            ),
-                                          ],
+                      return AlertDialog(
+                        content: Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(bottom: 0),
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        WidgetSpan(
+                                          child: Icon(
+                                            Icons.person,
+                                            size: 14,
+                                            color: Colors.deepPurple,
+                                          ),
                                         ),
-                                      ),
-                                    ),
-
-
-                                    Padding(
-                                      padding: EdgeInsets.only(bottom: 0),
-                                      child: RichText(
-                                        text: TextSpan(
-                                          children: [
-                                            WidgetSpan(
-                                              child: Icon(
-                                                Icons.email,
-                                                size: 14,
-                                                color: Colors.deepPurple,
-                                              ),
-                                            ),
-                                            TextSpan(
-                                              text: ("  Email- " + email),
-                                              style: TextStyle(
-                                                  color: Colors.black87,
-                                                  fontSize: 15,
-                                                  fontWeight: FontWeight.normal),
-                                            ),
-                                          ],
+                                        TextSpan(
+                                          text: ("  Owner Name- " + name),
+                                          style: TextStyle(
+                                              color: Colors.black87,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.normal),
                                         ),
-                                      ),
+                                      ],
                                     ),
-                                    TextFormField(
-                                      controller: _textEditingControllername,
-                                      style: TextStyle(
-                                          fontFamily: 'Montserrat',
-                                          fontSize: 13.0),
-                                      decoration: InputDecoration(
-                                          contentPadding: EdgeInsets.fromLTRB(
-                                              20.0, 15.0, 20.0, 15.0),
-                                          hintText: "Type Your Name",
-                                          border: OutlineInputBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(32.0))),
-                                    ),
-                                    SizedBox(height: 20.0),
-                                    TextFormField(
-                                      controller: _textEditingControlleremail,
-
-                                      style: TextStyle(
-                                          fontFamily: 'Montserrat',
-                                          fontSize: 13.0),
-                                      decoration: InputDecoration(
-                                          contentPadding: EdgeInsets.fromLTRB(
-                                              20.0, 15.0, 20.0, 15.0),
-                                          hintText: "Type Your Email",
-                                          border: OutlineInputBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(32.0))),
-                                    ),
-                                    SizedBox(height: 20.0),
-                                    TextFormField(
-                                      controller: _textEditingControllermessage,
-                                      keyboardType: TextInputType.multiline,
-                                      maxLines: 4,
-                                      style: TextStyle(
-                                          fontFamily: 'Montserrat',
-                                          fontSize: 13.0),
-                                      decoration: InputDecoration(
-                                          contentPadding: EdgeInsets.fromLTRB(
-                                              20.0, 15.0, 20.0, 15.0),
-                                          hintText: "Type your Message",
-                                          border: OutlineInputBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(32.0))),
-                                    ),
-                                  ],
+                                  ),
                                 ),
-                              )),
 
 
-                          title: Text(' Contact Form',
-                            textAlign: TextAlign.center,  style: TextStyle(
+                                Padding(
+                                  padding: EdgeInsets.only(bottom: 0),
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        WidgetSpan(
+                                          child: Icon(
+                                            Icons.email,
+                                            size: 14,
+                                            color: Colors.deepPurple,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: ("  Email- " + email),
+                                          style: TextStyle(
+                                              color: Colors.black87,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.normal),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                TextFormField(
+                                  controller: _textEditingControllername,
+                                  style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 13.0),
+                                  decoration: InputDecoration(
+                                      contentPadding: EdgeInsets.fromLTRB(
+                                          20.0, 15.0, 20.0, 15.0),
+                                      hintText: "Type Your Name",
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(32.0))),
+                                ),
+                                SizedBox(height: 20.0),
+                                TextFormField(
+                                  controller: _textEditingControlleremail,
 
-                              decoration: TextDecoration.underline,
-                                decorationStyle: TextDecorationStyle.double,
-                                decorationThickness: 1,
-                                decorationColor: Colors.green,
-
-
+                                  style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 13.0),
+                                  decoration: InputDecoration(
+                                      contentPadding: EdgeInsets.fromLTRB(
+                                          20.0, 15.0, 20.0, 15.0),
+                                      hintText: "Type Your Email",
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(32.0))),
+                                ),
+                                SizedBox(height: 20.0),
+                                TextFormField(
+                                  controller: _textEditingControllermessage,
+                                  keyboardType: TextInputType.multiline,
+                                  maxLines: 4,
+                                  style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 13.0),
+                                  decoration: InputDecoration(
+                                      contentPadding: EdgeInsets.fromLTRB(
+                                          20.0, 15.0, 20.0, 15.0),
+                                      hintText: "Type your Message",
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(32.0))),
+                                ),
+                              ],
                             )),
-                          actions: <Widget>[
-                            Container(
-                              margin: EdgeInsets.only(bottom: 20, right: 30),
-                              child: FlatButton(
-                                shape: RoundedRectangleBorder(
-                                    side: BorderSide(
-                                        color: Colors.green,
-                                        width: 1,
-                                        style: BorderStyle.solid),
-                                    borderRadius: BorderRadius.circular(50)),
-                                child: Text(
-                                  'Send',
-                                  style: TextStyle(fontSize: 15.0),
-                                ),
-                                color: Colors.green,
-                                textColor: Colors.white,
-                                onPressed: () {
-                                  print(_textEditingControllermessage);
-                                  print(_textEditingControllername);
-                                  print(_textEditingControlleremail);
-                                  Navigator.of(context).pop();
-                                },
+
+
+                        title: Text(' Contact Form',
+                          textAlign: TextAlign.center,  style: TextStyle(
+
+                            decoration: TextDecoration.underline,
+                              decorationStyle: TextDecorationStyle.double,
+                              decorationThickness: 1,
+                              decorationColor: Colors.green,
+
+
+                          )),
+                        actions: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(bottom: 20, right: 30),
+                            child: FlatButton(
+                              shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      color: Colors.green,
+                                      width: 1,
+                                      style: BorderStyle.solid),
+                                  borderRadius: BorderRadius.circular(50)),
+                              child: Text(
+                                'Send',
+                                style: TextStyle(fontSize: 15.0),
                               ),
-                            )
-                          ],
-                        ),
+                              color: Colors.green,
+                              textColor: Colors.white,
+                              onPressed: () {
+                                sendMail();
+                                print(_textEditingControllermessage.text);
+                                print(_textEditingControllername.text);
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          )
+                        ],
                       );
                     });
               },
